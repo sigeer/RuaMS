1fb0225cef21365c72ac862408156a084e211650
sigeer <1452069418@qq.com>
2024-10-15 10:11:44 +0800

    测试：提供单独的MockupClient，用于辅助测试在线角色
    `Client.createMock()`继续保留，用于测试Client

---
+++ src/Application.Core/Game/MockupClient.cs
@@ -0,0 +1,556 @@
+﻿using Application.Core.Game.Life;
+using Application.Core.Game.TheWorld;
+using Application.Core.Scripting.Infrastructure;
+using DotNetty.Handlers.Timeout;
+using DotNetty.Transport.Channels;
+using net.packet;
+using net.server.channel;
+using net.server.coordinator.session;
+using scripting;
+using scripting.Event;
+using scripting.npc;
+using scripting.quest;
+
+namespace Application.Core.Game
+{
+    public class MockupClient : IClient
+    {
+        IPlayer? mockPlayer;
+        public IPlayer? Character => mockPlayer;
+
+        public string ClientInfo => "MockupClient";
+        int _world;
+        int _channel;
+
+        public MockupClient(int world, int channel)
+        {
+            _world = world;
+            _channel = channel;
+        }
+
+        public MockupClient() : this(0, 1) { }
+
+        public bool acceptToS()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void addVotePoints(int points)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void announceBossHpBar(Monster mm, int mobHash, Packet packet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void announceHint(string msg, int length)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void announceServerMessage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool attemptCsCoupon()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void banHWID()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void banMacs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool canBypassPic()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool canBypassPin()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool canClickNPC()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool canGainCharacterSlot()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool canRequestCharlist()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void changeChannel(int channel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool checkBirthDate(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void checkChar(int accid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void checkIfIdle(IdleStateEvent evt)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool checkPic(string other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool checkPin(string other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void closePlayerScriptInteractions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void closeSession()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool deleteCharacter(int cid, int senderAccId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void disconnect(bool shutdown, bool cashshop)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void disconnectSession()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void enableCSActions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExceptionCaught(IChannelHandlerContext ctx, Exception cause)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int finishLogin()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void forceDisconnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool gainCharacterSlot()
+        {
+            throw new NotImplementedException();
+        }
+
+        public AbstractPlayerInteraction getAbstractPlayerInteraction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int getAccID()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getAccountName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public short getAvailableCharacterSlots()
+        {
+            throw new NotImplementedException();
+        }
+
+        public short getAvailableCharacterWorldSlots()
+        {
+            throw new NotImplementedException();
+        }
+
+        public short getAvailableCharacterWorldSlots(int world)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int getChannel()
+        {
+            return 0;
+        }
+
+        IWorldChannel? _channelServer;
+        public IWorldChannel getChannelServer()
+        {
+            return _channelServer ??= new WorldChannel(getWorldServer(), _channel, 0);
+        }
+
+        public IWorldChannel getChannelServer(byte channel)
+        {
+            return new WorldChannel(getWorldServer(), channel, 0);
+        }
+
+        public short getCharacterSlots()
+        {
+            throw new NotImplementedException();
+        }
+
+        public NPCConversationManager? getCM()
+        {
+            throw new NotImplementedException();
+        }
+
+        public EventManager getEventManager(string evt)
+        {
+            throw new NotImplementedException();
+        }
+
+        public sbyte getGender()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int getGMLevel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public sbyte getGReason()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Hwid getHwid()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int getLanguage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public long getLastPacket()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int getLoginState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public HashSet<string> getMacs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getPic()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getPin()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPlayer? getPlayer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public QuestActionManager? getQM()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getRemoteAddress()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEngine? getScriptEngine(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long getSessionId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTimeOffset? getTempBanCalendar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTimeOffset? getTempBanCalendarFromDB()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int getVisibleWorlds()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int getVotePoints()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int getWorld()
+        {
+            return _world;
+        }
+
+        IWorld _worldServer;
+        public IWorld getWorldServer()
+        {
+            return _worldServer ??= new World(_world, 0, "", 1, 1, 1, 1, 1, 1, 1);
+        }
+
+        public bool hasBannedHWID()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool hasBannedIP()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool hasBannedMac()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool hasBeenBanned()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool isInTransition()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool isLoggedIn()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<IPlayer> loadCharacters(int serverId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void lockClient()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int login(string login, string pwd, Hwid nibbleHwid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void pongReceived()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void releaseClient()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void removeClickedNPC()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void removeScriptEngine(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void requestedServerlist(int worlds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void resetCsCoupon()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void sendCharList(int server)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void sendPacket(Packet packet)
+        {
+            Console.WriteLine("SendPacket");
+            return;
+        }
+
+        public void setAccID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setAccountName(string a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setChannel(int channel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setCharacterOnSessionTransitionState(int cid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setCharacterSlots(sbyte slots)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setClickedNPC()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setGender(sbyte m)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setGMLevel(int level)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setHwid(Hwid? hwid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setLanguage(int lingua)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setPic(string pic)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setPin(string pin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setPlayer(IPlayer? player)
+        {
+            mockPlayer = player;
+        }
+
+        public void setScriptEngine(string name, IEngine e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setWorld(int world)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool tryacquireClient()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool tryacquireEncoder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void unlockClient()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void unlockEncoder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updateHwid(Hwid hwid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updateLastPacket()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updateLoginState(int newState)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updateMacs(string macData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UserEventTriggered(IChannelHandlerContext ctx, object evt)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void useVotePoints(int points)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- test/ServiceTest/TestBase.cs
+++ test/ServiceTest/TestBase.cs
@@ -1,6 +1,12 @@
-﻿using Application.Core.Game;
+﻿using Application.Core.Compatible;
+using Application.Core.Game;
+using Application.Core.Game.Players;
 using Application.Core.Managers;
+using Application.Core.scripting.Event.jobs;
+using constants.id;
 using net.server;
+using Quartz.Impl;
+using server.maps;
 
 namespace ServiceTest
 {
@@ -12,10 +18,23 @@ namespace ServiceTest
         }
         protected static IClient GetOnlinedTestClient(int charId = 1)
         {
+            var factory = new StdSchedulerFactory();
+            SchedulerManage.Scheduler = factory.GetScheduler().Result;
+            SchedulerManage.Scheduler.ListenerManager.AddJobListener(new JobCompleteListener());
+
             Server.getInstance().forceUpdateCurrentTime();
-            var mockClient = Client.createMock();
-            mockClient.setPlayer(CharacterManager.GetPlayerById(charId));
+            var mockClient = new MockupClient();
+            GetMockPlayer(mockClient, charId);
             return mockClient;
+        }
+
+        protected static IPlayer GetMockPlayer(IClient client, int charId = 1)
+        {
+            var player = CharacterManager.GetPlayerById(charId)!;
+            client.setPlayer(player);
+            player.setClient(client);
+            player.setMap(new MapManager(null, 0, 1).getMap(MapId.HENESYS));
+            return player;
         }
     }
 }
