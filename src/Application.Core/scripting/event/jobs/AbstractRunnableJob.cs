df32fae1cde0be352c13dfb1da5dc5ab20c3a1ef
sigeer <1452069418@qq.com>
2024-10-17 15:14:00 +0800

    其他：延迟、定时任务日志打印

--- src/Application.Core/scripting/event/jobs/AbstractRunnableJob.cs
+++ src/Application.Core/scripting/event/jobs/AbstractRunnableJob.cs
@@ -10,9 +10,12 @@ namespace Application.Core.scripting.Event.jobs
             var type = context.JobDetail.JobDataMap[JobDataKeys.Data];
 
             if (type is AbstractRunnable r)
+            {
+                Log.Logger.Debug($"TaskJob  {type.GetType().Name}.{r.Name}");
                 r.run();
+            }
             else
-                Log.Logger.Debug($"TaskJob  {(type == null ? "null" : type.GetType().Name)}");
+                Log.Logger.Error($"TaskJob Invalid, {(type == null ? "null" : type.GetType().Name)}");
             return Task.CompletedTask;
         }
     }
