c160bfda8eda09e73327e3de3902ace4bc51849f
sigeer <1452069418@qq.com>
2024-10-18 10:45:32 +0800

    撤销：不打印任务dbg日志

--- src/Application.Core/scripting/event/jobs/AbstractRunnableJob.cs
+++ src/Application.Core/scripting/event/jobs/AbstractRunnableJob.cs
@@ -10,10 +10,7 @@ namespace Application.Core.scripting.Event.jobs
             var type = context.JobDetail.JobDataMap[JobDataKeys.Data];
 
             if (type is AbstractRunnable r)
-            {
-                Log.Logger.Debug($"TaskJob  {type.GetType().Name}.{r.Name}");
                 r.run();
-            }
             else
                 Log.Logger.Error($"TaskJob Invalid, {(type == null ? "null" : type.GetType().Name)}");
             return Task.CompletedTask;
