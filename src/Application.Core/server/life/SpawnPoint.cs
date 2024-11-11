1a6b2a69cd91686069940bbe9360571698df2faf
sigeer <1452069418@qq.com>
2024-11-11 21:48:26 +0800

    修复

--- src/Application.Core/server/life/SpawnPoint.cs
+++ src/Application.Core/server/life/SpawnPoint.cs
@@ -152,10 +152,10 @@ public class SpawnPoint
 
     public void SpawnMonster(int difficulty = 1, bool isPq = false)
     {
-        var rate =_map.MonsterRate;
+        var rate =_map.MonsterRate * _map.getWorldServer().MobRate;
         // 全局倍率也可以在这里控制，但是全局倍率应该对有事件的地图除外 _map.getEventInstance() != null
-        if (_map.getEventInstance() == null)
-            rate *= _map.getWorldServer().MobRate;
+        if (_map.getEventInstance() != null)
+            rate = 1;
 
         if (_monsterMeta.isBoss())
             rate = 1;
