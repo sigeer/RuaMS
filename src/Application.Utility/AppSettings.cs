7988def2583752f9dd1f1b751b2059f6654388da
sigeer <1452069418@qq.com>
2025-02-12 09:04:50 +0800

    优化：允许环境变量覆盖配置文件

--- src/Application.Utility/AppSettings.cs
+++ src/Application.Utility/AppSettings.cs
@@ -4,6 +4,5 @@ namespace Application.Utility
 {
     public class AppSettings
     {
-        public readonly static string GetConnectionString = Environment.GetEnvironmentVariable("DB_CONNECTIONSTRING") ?? YamlConfig.config.server.DB_CONNECTIONSTRING;
     }
 }
