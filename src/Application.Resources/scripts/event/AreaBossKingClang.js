51802dadbb81dfdf82bda03918ef4ffc2c497eae
sigeer <1452069418@qq.com>
2025-04-11 17:22:51 +0800

    回滚

--- src/Application.Resources/scripts/event/AreaBossKingClang.js
+++ src/Application.Resources/scripts/event/AreaBossKingClang.js
@@ -1,4 +1,33 @@
-﻿var hotSand;
+/*
+    This file is part of the OdinMS Maple Story Server
+    Copyright (C) 2008 Patrick Huy <patrick.huy@frz.cc>
+               Matthias Butz <matze@odinms.de>
+               Jan Christian Meyer <vimes@odinms.de>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation version 3 as published by
+    the Free Software Foundation. You may not use, modify or distribute
+    this program under any other version of the GNU Affero General Public
+    License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+ -- Odin JavaScript --------------------------------------------------------------------------------
+ King Clang Spawner
+ -- Edited by --------------------------------------------------------------------------------------
+ ThreeStep - based on xQuasar's King Clang spawner
+
+ **/
+
+var hotSand;
 
 function init() {
     hotSand = em.getChannelServer().getMapFactory().getMap(110040000);
@@ -32,33 +61,32 @@ function start() {
 
 // ---------- FILLER FUNCTIONS ----------
 
-function dispose() {}
+function dispose() { }
 
-function setup(eim, leaderid) {}
+function setup(eim, leaderid) { }
 
-function monsterValue(eim, mobid) {return 0;}
+function monsterValue(eim, mobid) { return 0; }
 
-function disbandParty(eim, player) {}
+function disbandParty(eim, player) { }
 
-function playerDisconnected(eim, player) {}
+function playerDisconnected(eim, player) { }
 
-function playerEntry(eim, player) {}
+function playerEntry(eim, player) { }
 
-function monsterKilled(mob, eim) {}
+function monsterKilled(mob, eim) { }
 
-function scheduledTimeout(eim) {}
+function scheduledTimeout(eim) { }
 
-function afterSetup(eim) {}
+function afterSetup(eim) { }
 
-function changedLeader(eim, leader) {}
+function changedLeader(eim, leader) { }
 
-function playerExit(eim, player) {}
+function playerExit(eim, player) { }
 
-function leftParty(eim, player) {}
+function leftParty(eim, player) { }
 
-function clearPQ(eim) {}
+function clearPQ(eim) { }
 
-function allMonstersDead(eim) {}
+function allMonstersDead(eim) { }
 
-function playerUnregistered(eim, player) {}
-
+function playerUnregistered(eim, player) { }
--- src/Application.Resources/scripts/event/BalrogBattle.js
+++ src/Application.Resources/scripts/event/BalrogBattle.js
@@ -1,4 +1,28 @@
-﻿
+/*
+    This file is part of the HeavenMS MapleStory Server
+    Copyleft (L) 2016 - 2019 RonanLana
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation version 3 as published by
+    the Free Software Foundation. You may not use, modify or distribute
+    this program under any other version of the GNU Affero General Public
+    License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * @author: Ronan
+ * @event: Vs Balrog
+ */
+
 var isPq = true;
 var minPlayers = 6, maxPlayers = 30;
 var minLevel = 50, maxLevel = 255;
@@ -112,7 +136,7 @@ function afterSetup(eim) {
     spawnBalrog(eim);
 }
 
-function respawnStages(eim) {}
+function respawnStages(eim) { }
 
 function releaseLeftClaw(eim) {
     eim.getInstanceMap(entryMap).killMonster(8830006);
@@ -138,7 +162,7 @@ function scheduledTimeout(eim) {
     end(eim);
 }
 
-function playerUnregistered(eim, player) {}
+function playerUnregistered(eim, player) { }
 
 function playerExit(eim, player) {
     eim.unregisterPlayer(player);
@@ -162,9 +186,9 @@ function changedMap(eim, player, mapid) {
     }
 }
 
-function changedLeader(eim, leader) {}
+function changedLeader(eim, leader) { }
 
-function playerDead(eim, player) {}
+function playerDead(eim, player) { }
 
 function playerRevive(eim, player) { // player presses ok on the death pop up.
     if (eim.isExpeditionTeamLackingNow(true, minPlayers, player)) {
@@ -184,9 +208,9 @@ function playerDisconnected(eim, player) {
     }
 }
 
-function leftParty(eim, player) {}
+function leftParty(eim, player) { }
 
-function disbandParty(eim) {}
+function disbandParty(eim) { }
 
 function monsterValue(eim, mobId) {
     return 1;
@@ -245,9 +269,8 @@ function monsterKilled(mob, eim) {
     }
 }
 
-function allMonstersDead(eim) {}
+function allMonstersDead(eim) { }
 
-function cancelSchedule() {}
+function cancelSchedule() { }
 
-function dispose(eim) {}
-
+function dispose(eim) { }
