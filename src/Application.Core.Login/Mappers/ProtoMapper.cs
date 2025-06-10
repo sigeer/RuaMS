bbdba5ebc0cb760e9a8110efcd8e4da219d09b8b
sigeer <1452069418@qq.com>
2025-06-10 17:30:24 +0800

    修复 mapper

--- src/Application.Core.Login/Mappers/ProtoMapper.cs
+++ src/Application.Core.Login/Mappers/ProtoMapper.cs
@@ -69,14 +69,14 @@ namespace Application.Core.Login.Mappers
 
             CreateMap<CharacterLiveObject, Dto.PlayerGetterDto>();
 
-            CreateMap<CharacterViewObject, Dto.TeamMemberDto>()
+            CreateMap<CharacterLiveObject, Dto.TeamMemberDto>()
                 .ForMember(dest => dest.Channel, src => src.MapFrom(x => x.Channel))
                 .ForMember(dest => dest.Id, src => src.MapFrom(x => x.Character.Id))
                 .ForMember(dest => dest.Name, src => src.MapFrom(x => x.Character.Name))
                 .ForMember(dest => dest.Job, src => src.MapFrom(x => x.Character.JobId))
                 .ForMember(dest => dest.Level, src => src.MapFrom(x => x.Character.Level));
 
-            CreateMap<CharacterViewObject, Dto.GuildMemberDto>()
+            CreateMap<CharacterLiveObject, Dto.GuildMemberDto>()
                 .ForMember(dest => dest.Channel, src => src.MapFrom(x => x.Channel))
                 .ForMember(dest => dest.Id, src => src.MapFrom(x => x.Character.Id))
                 .ForMember(dest => dest.Name, src => src.MapFrom(x => x.Character.Name))
