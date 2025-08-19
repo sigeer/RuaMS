d19725ea60afd9ba03a0d88841d469fe049a500e
sigeer <1452069418@qq.com>
2025-08-19 22:44:30 +0800

    更换MysqlProvider版本后需要更新迁移

--- src/Application.Core.EF.MySQL/Migrations/20250819143922_Update0819.Designer.cs
+++ src/Application.Core.EF.MySQL/Migrations/20250819143922_Update0819.Designer.cs
@@ -4,6 +4,7 @@ using Application.EF;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Application.Core.EF.MySQL.Migrations
 {
     [DbContext(typeof(DBContext))]
-    partial class DBContextModelSnapshot : ModelSnapshot
+    [Migration("20250819143922_Update0819")]
+    partial class Update0819
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
