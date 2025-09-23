c0fbe1ff16a51937aa042d558e3e22e06125c474
sigeer <1452069418@qq.com>
2025-09-23 09:03:04 +0800

    更新：完善string.wz测试及修复

--- src/Application.Templates.XmlWzReader/Provider/StringSkillProvider.cs
+++ src/Application.Templates.XmlWzReader/Provider/StringSkillProvider.cs
@@ -1,4 +1,5 @@
 using Application.Templates.String;
+using System.Xml.Linq;
 
 namespace Application.Templates.XmlWzReader.Provider
 {
@@ -6,6 +7,27 @@ namespace Application.Templates.XmlWzReader.Provider
     {
         public StringSkillProvider(TemplateOptions options) : base(options, [StringTemplateType.Skill])
         {
+        }
+
+        protected override AbstractTemplate? SetStringTemplate(XElement rootNode)
+        {
+            var skillIdStr = rootNode.GetName();
+            if (string.IsNullOrEmpty(skillIdStr) || skillIdStr.Length < 7)
+                return null;
+
+            if (int.TryParse(skillIdStr, out var id))
+            {
+                var template = new StringTemplate(id);
+                foreach (var propNode in rootNode.Elements())
+                {
+                    var infoPropName = propNode.GetName();
+                    if (infoPropName == "name")
+                        template.Name = propNode.GetStringValue() ?? "";
+                }
+                InsertItem(template);
+                return template;
+            }
+            return null;
         }
     }
 }
