a1577953b9f7f53b7328757a8af9dfb0457e7b2c
sigeer <1452069418@qq.com>
2026-05-21 14:40:21 +0800

    更新：视野机制
    - 修复屏蔽命令
    - 调整视野范围

--- src/Application.Core/Game/Maps/AbstractMapObject.cs
+++ src/Application.Core/Game/Maps/AbstractMapObject.cs
@@ -97,11 +97,29 @@ public abstract class AbstractMapObject : IMapObject
     /// </summary>
     /// <param name="chr"></param>
     /// <returns></returns>
-    protected virtual bool IsVisibleForPlayerWithoutRange(Player chr) => MapModel == chr.MapModel;
+    protected virtual bool IsVisibleForPlayerWithoutRange(Player chr)
+    {
+        if (MapModel != chr.MapModel)
+        {
+            return false;
+        }
+
+        if (getType() == MapObjectType.SUMMON && chr.HideSummon)
+        {
+            return false;
+        }
+
+        if (getType() == MapObjectType.PET && chr.HidePet)
+        {
+            return false;
+        }
+
+        return true;
+    }
     public virtual bool IsVisibleForPlayer(Player chr)
     {
-        return IsVisibleForPlayerWithoutRange(chr) && (!MapModel.UseRangedView
-            || MapGlobalData.IsObjectInRange(this, chr.getPosition(), MapModel.ChannelServer.NodeService.ServerConfig.SystemConfig.GetRangedDistance()));
+        return IsVisibleForPlayerWithoutRange(chr) && 
+            (!MapModel.UseRangedView || MapGlobalData.IsObjectInRange(this, chr.getPosition(), MapModel.ChannelServer.NodeService.ServerConfig.SystemConfig.GetRangedDistance()));
     }
 
 
@@ -114,15 +132,6 @@ public abstract class AbstractMapObject : IMapObject
                 continue;
             }
 
-            if (getType() == MapObjectType.SUMMON && mapChr.HideSummon)
-            {
-                continue;
-            }
-
-            if (getType() == MapObjectType.PET && mapChr.HidePet)
-            {
-                continue;
-            }
 
             if (MapModel.IsMapObjectVisibleForPlayerCached(mapChr, this))
             {
